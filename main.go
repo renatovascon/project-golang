711ff479bf4a9d1224aae072224749c9a61769dd
Renato Vasconcelos Coura Soares <renato@Renatos-MacBook-Pro.local>
2023-11-22 23:16:36 -0300

    fix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com.br/renatovascon/project-golang/internal/infra/database"
 	"github.com.br/renatovascon/project-golang/internal/usecase"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
